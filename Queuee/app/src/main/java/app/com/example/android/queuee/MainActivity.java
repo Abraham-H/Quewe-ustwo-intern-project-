55b4f0720cacc1832791385ca04938bce7d79fd3
Boris Kachscovsky <kachscovsky@gmail.com>
2015-08-31 14:06:21 +0200

    switched to queue on firebase

--- Queuee/app/src/main/java/app/com/example/android/queuee/MainActivity.java
+++ Queuee/app/src/main/java/app/com/example/android/queuee/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
 
     public void setupFirebase(){
         Firebase.setAndroidContext(this);
-        mRef = new Firebase("https://burning-torch-3063.firebaseio.com/condition");
+        mRef = new Firebase("https://burning-torch-3063.firebaseio.com/queue");
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
