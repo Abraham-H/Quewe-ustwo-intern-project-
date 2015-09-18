6ae0ce8e8c9fe4eadc3172e159e50d29cb881f6f
Boris Kachscovsky <kachscovsky@gmail.com>
2015-09-18 13:13:23 +0200

    Changed message class

--- Queuee2/app/src/main/java/app/com/example/android/queuee2/model/Response.java
+++ Queuee2/app/src/main/java/app/com/example/android/queuee2/model/Response.java
@@ -5,11 +5,22 @@ package app.com.example.android.queuee2.model;
  */
 public class Response{
 
-    public static class Message{
-        public Message (String message){setMessage(message);}
+    public static class Message {
+        public Message(String message){
+            this(message,false);
+        }
+        public Message(String message, boolean error){
+            setMessage(message);
+            setError(error);
+        }
+
         private String message;
+        private boolean error;
+
+        public boolean isError() {return error;}
+        public void setError(boolean error) {this.error = error;}
         public String getMessage() {return message;}
-        public void setMessage(String message){this.message = message;}
+        public void setMessage(String message) {this.message = message;}
     }
 
     public static class QueueData{
